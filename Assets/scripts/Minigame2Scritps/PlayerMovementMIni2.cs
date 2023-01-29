50499fbe1151022f90d21c4c532bd9f1258acb3a
BubuBiB00 <florentin.aldrian@ms.bulme.at>
2023-01-29 15:29:56 +0100

    added jumping and animations

--- Assets/scripts/Minigame2Scritps/PlayerMovementMIni2.cs
+++ Assets/scripts/Minigame2Scritps/PlayerMovementMIni2.cs
@@ -1,15 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerMovementMIni2 : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    [SerializeField] private float jump_height;
 
     private Rigidbody2D body;
+    private Animator anim;
+    
 
     private void Awake()
     {
+        // Grab references from game objects
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        // Flip player when moving in different directions
+        if(horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if(Input.GetKey(KeyCode.Space)) 
+        {
+            body.velocity = new Vector2(body.velocity.x, speed);
+        }
+
+        // Set animator parameters
+        anim.SetBool("Run", horizontalInput != 0);
+    }
 }
