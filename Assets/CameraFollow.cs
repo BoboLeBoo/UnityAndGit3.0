8e7c866ddb835909e022a2ecfd0962026e1df0af
BubuBiB00 <florentin.aldrian@ms.bulme.at>
2023-01-31 20:38:05 +0100

    updated platforms

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -6,14 +6,15 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothSpeed = 0.3f;
+
+    private Vector3 currentVelocity;
     // Update is called once per frame
     void LateUpdate()
     {
         if(target.position.y > transform.position.y)
         {
             Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
-            transform.position = Vector3.Lerp(transform.position, newPos, smoothSpeed);
+            transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothSpeed * Time.deltaTime);
         }
     }
-
 }
