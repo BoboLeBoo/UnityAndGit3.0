f33abb7ab86ed16935deab901bc5469a25188308
BubuBiB00 <florentin.aldrian@ms.bulme.at>
2023-02-01 20:37:04 +0100

    add side to side teleportation

--- Assets/scripts/Minigame2Scritps/PlayerMovementMIni2.cs
+++ Assets/scripts/Minigame2Scritps/PlayerMovementMIni2.cs
@@ -1,5 +1,9 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,8 +11,11 @@ public class PlayerMovementMIni2 : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
     [SerializeField] private float jump_height;
+    [SerializeField] public BoxCollider2D TeleborderL;
+    [SerializeField] public BoxCollider2D TeleborderR;
 
     private bool grounded;
+    private string lastTeleport = "";
 
     private Rigidbody2D body;
     private Animator anim;
@@ -45,6 +52,11 @@ public class PlayerMovementMIni2 : MonoBehaviour
         anim.SetBool("grounded", grounded);
     }
 
+    private void FixedUpdate() //move physics related stuff to this funkction
+    {
+        
+    }
+
     private void Jump()
     {
         body.velocity = new Vector2(body.velocity.x, jump_height);
@@ -54,9 +66,26 @@ public class PlayerMovementMIni2 : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag == "Ground")
         {
             grounded = true;
+            lastTeleport = "";
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // this teleports the player to the other side of the screen, when he leaves on one side
+        // add changed direction method, so that u can pass by changing direction of the player
+        if (collision.gameObject.name == TeleborderL.name && collision.gameObject.name != lastTeleport || collision.gameObject.name == "")
+        {
+            body.position = new Vector3(TeleborderR.transform.position.x, TeleborderR.transform.position.y, movementSpeed);
+            lastTeleport = TeleborderR.name;
+        }
+        if (collision.gameObject.name == TeleborderR.name && collision.gameObject.name != lastTeleport || collision.gameObject.name == "")
+        {
+            body.position = new Vector3(TeleborderL.transform.position.x, TeleborderL.transform.position.y, movementSpeed);
+            lastTeleport = TeleborderL.name;
         }
     }
 }
