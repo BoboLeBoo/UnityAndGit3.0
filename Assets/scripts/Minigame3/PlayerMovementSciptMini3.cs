219c6b1ca3a375a0425c6107e74fdd577f7cf5bc
BoboLeBoo <lorenz.fritz@bulme.at>
2023-02-03 16:28:25 +0100

    finished movement minigame 3 and fixed bugs

--- Assets/scripts/Minigame3/PlayerMovementSciptMini3.cs
+++ Assets/scripts/Minigame3/PlayerMovementSciptMini3.cs
@@ -7,11 +7,15 @@ public class PlayerMovementSciptMini3 : MonoBehaviour
   
 
     [SerializeField] private Rigidbody2D player;
+    [SerializeField] private BoxCollider2D playerCollider;
+    [SerializeField] private LayerMask jumpableGround;
     private Animator anim;
     private float dirX = 0;
     private SpriteRenderer sprite;
     [SerializeField] private float moveSpeed = 7;
     [SerializeField] private float jumpForce = 14;
+    private  enum MovementState{ idle, running, jumping, falling  }
+   
     
 
     void Start()
@@ -26,7 +30,7 @@ public class PlayerMovementSciptMini3 : MonoBehaviour
 
         player.velocity = new Vector2(dirX * moveSpeed, player.velocity.y);
 
-        if (Input.GetButtonDown("Jump") == true)
+        if (Input.GetButtonDown("Jump") && IsGrounded() == true)
         {
             player.velocity = new Vector2(player.velocity.x,jumpForce);
      
@@ -39,21 +43,41 @@ public class PlayerMovementSciptMini3 : MonoBehaviour
 
     private void UpdateAnimationState()
     {
+        MovementState state;
+
         if (dirX > 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = false;
         }
 
         else if (dirX < 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = true;
         }
 
         else
         {
-            anim.SetBool("running", false);
+            state = MovementState.idle;
         }
+
+        if (player.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+
+        else if (player.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
     }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0f , Vector2.down, .1f, jumpableGround);
+    }
+
 }
