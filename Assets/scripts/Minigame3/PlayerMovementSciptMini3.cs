9b38159cf46fdea39ab5d8c73a2a8fa6f166a49b
BoboLeBoo <lorenz.fritz@bulme.at>
2023-02-03 13:10:20 +0100

    added animations

--- Assets/scripts/Minigame3/PlayerMovementSciptMini3.cs
+++ Assets/scripts/Minigame3/PlayerMovementSciptMini3.cs
@@ -7,22 +7,53 @@ public class PlayerMovementSciptMini3 : MonoBehaviour
   
 
     [SerializeField] private Rigidbody2D player;
+    private Animator anim;
+    private float dirX = 0;
+    private SpriteRenderer sprite;
+    [SerializeField] private float moveSpeed = 7;
+    [SerializeField] private float jumpForce = 14;
     
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
 
-        player.velocity = new Vector2(dirX * 7f, player.velocity.y);
+        player.velocity = new Vector2(dirX * moveSpeed, player.velocity.y);
 
         if (Input.GetButtonDown("Jump") == true)
         {
-            player.velocity = new Vector2(player.velocity.x,14);
+            player.velocity = new Vector2(player.velocity.x,jumpForce);
      
-            
         }
-  
+
+        UpdateAnimationState();
+
     }
 
 
+    private void UpdateAnimationState()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+
+        else
+        {
+            anim.SetBool("running", false);
+        }
+    }
 }
